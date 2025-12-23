7877cdaef7d5c0fb2629f9ba4614509d39e51907
catalinzisu <catalinzisu913@gmail.com>
2025-12-23 19:01:13 +0200

    created the scheme for the whole project

---
+++ BE-springboot/src/test/java/org/example/bespringboot/BeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
